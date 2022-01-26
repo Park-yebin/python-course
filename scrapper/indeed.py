555e8ff9086227e02b81ee66bb3617d40732b553
Park-yebin <81156273+Park-yebin@users.noreply.github.com>
2022-01-26 22:11:54 +0900

    scrapping things
    getting id, location, company name yeah

--- scrapper/indeed.py
+++ scrapper/indeed.py
@@ -21,13 +21,32 @@ def extract_indeed_pages():
     return max_page
 
 
+def extract_job(result):
+    title = result.select_one('.jobTitle>span').string
+    company = result.find("div", {"class": "company_location"})
+    company_span = company.find("span", {"class": "companyName"})
+    company_anchor = company.find("a")
+    if company_anchor is not None:
+        # 빈칸이 있는 경우 company = company.strip() 하면 불필요한 빈칸들이 없어짐
+        company = str(company_anchor.string)
+    elif company_span is not None:
+        company = str(company_span.string)
+    else:
+        company = "None"
+    location = result.find("div", {"class": "companyLocation"}).string
+    # 받아온 곳(result)에서의 범위 안에 존재하지 않으면 오류난다. 범위 설정 조심
+    job_id = result['data-jk']
+    return {'title': title, 'company': company, 'location': location, 'link': f"https://kr.indeed.com/viewjob?jk={job_id}&tk=1fqb29bmvk47h801&from=serp&vjs=3"}
+
+
 def extract_indeed_jobs(last_page):
     jobs = []
-    # for page in range(last_page):
-    result = requests.get(f"{URL}&start={0*LIMIT}")
-    soup = BeautifulSoup(result.text, "html.parser")
-    results = soup.find_all("div", {"class": "job_seen_beacon"})
-    for result in results:
-        title = result.find("h2", {"class": "jobTitle"}).find("span").string
-        print(title)
+    for page in range(last_page):
+        print(f"Scrapping page {page}")
+        result = requests.get(f"{URL}&start={page*LIMIT}")
+        soup = BeautifulSoup(result.text, "html.parser")
+        results = soup.find_all("a", {"class": "resultWithShelf"})
+        for result in results:
+            job = extract_job(result)
+            jobs.append(job)
     return jobs
