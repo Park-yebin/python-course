7440a16f7f836dba424e5c75ebf3b2b3de564422
Park-yebin <81156273+Park-yebin@users.noreply.github.com>
2022-01-26 20:15:56 +0900

    Find job info
    find job info in one page

--- scrapper/indeed.py
+++ scrapper/indeed.py
@@ -1,11 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 
-INDEED_URL = "https://kr.indeed.com/jobs?q=python&limit=50&radius=25&vjk=e7c8c2b11f24fb43"
+LIMIT = 50
+URL = f"https://kr.indeed.com/jobs?q=python&limit={LIMIT}&radius=25&vjk=e7c8c2b11f24fb43"
 
 
 def extract_indeed_pages():
-    result = requests.get(INDEED_URL)
+    result = requests.get(URL)
 # "https://kr.indeed.com/jobs?q=python&limit=50&radius=25&start=50&vjk=8f62684fb1d35c1d"
     soup = BeautifulSoup(result.text, "html.parser")
     pagination = soup.find("div", {"class": "pagination"})
@@ -18,3 +19,13 @@ def extract_indeed_pages():
 
     max_page = pages[-1]
     return max_page
+
+
+def extract_indeed_jobs(last_page):
+    jobs = []
+    for page in range(last_page):
+        result = requests.get(f"{URL}&start={page*LIMIT}")
+        soup = BeautifulSoup(result.text, "html.parser")
+        results = soup.find_all("div", {"class": "job_seen_beacon"})
+        print(results)
+    return jobs
