eee3e3e0b0b3dbdf4ff36605cc717af6fcecad19
Park-yebin <81156273+Park-yebin@users.noreply.github.com>
2022-01-28 17:49:11 +0900

    CSV
    save with csv

--- scrapper/scrapping.py
+++ scrapper/scrapping.py
@@ -1,7 +1,8 @@
 from indeed import get_jobs as get_indeed_jobs
 from stockoverflow import get_jobs as get_sto_jobs
+from save import save_to_file
 
 indeed_jobs = get_indeed_jobs()
 stock_jobs = get_sto_jobs()
 jobs = stock_jobs + indeed_jobs
-print(jobs)
+save_to_file(jobs)
